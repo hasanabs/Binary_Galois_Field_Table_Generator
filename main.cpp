5220f138f67bb79368c9e398a55a81459a891589
Hasan Albinsaid <hasanmlg@gmail.com>
2019-12-25 20:49:57 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,6 @@ vector <int> primitive_poly {1,1,1,0,1}; //x^5 = 1 + x + x^2 + x^4
 int main()
 {
     cout << "     Poly Table    "<<endl;
-
     cout <<"GF(2^"<<GF<< ") |<LSB- -MSB>" << endl;
     vector<vector<int>> poly_table = hasan.primitive_poly_table_bin(GF,primitive_poly); //Generate representation GF(2^x)
     for (int i=0;i<pow(2,GF);i++){
